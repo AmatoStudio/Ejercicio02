3570708028669e58267109d63eaf9f229abbbdb6
AmatoStudio <lui.mezaf@duocuc.cl>
2024-06-12 09:36:55 -0400

    Update 01_edad_Equipo.py

--- 01_edad_Equipo.py
+++ 01_edad_Equipo.py
@@ -4,11 +4,11 @@ print("El numero que acaba de dar es su", edad)
 equipo = input("Digite su equipo, colo_colo_catolica u otro: ")
 print("El equipo que elegiste es", equipo)
 
-if (0>50 and "colo colo"):
+if (0>49 and "colo colo"):
     print("Tienes un gran futuro")
-elif 0>50 == "Catolica":
+elif 0>49 == "Catolica":
     print("Siempre estaras segundo")
-elif 0>50 and "otro":
+elif 0>49 and "otro":
     print("Lo siento mucho")
-elif 0<50 and "colo colo":
+elif 0<49 and "colo colo":
     print("Has tenido una bella vida")
