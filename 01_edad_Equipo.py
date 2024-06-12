cf9092f2d8bc0f2200f38715e26d3e660a0dd95e
AmatoStudio <lui.mezaf@duocuc.cl>
2024-06-12 09:31:45 -0400

    Add files via upload

---
+++ 01_edad_Equipo.py
@@ -0,0 +1,14 @@
+
+edad = input ("Digite su edad: ")
+print("El numero que acaba de dar es su", edad)
+equipo = input("Digite su equipo, colo_colo_catolica u otro: ")
+print("El equipo que elegiste es", equipo)
+
+if (0>50 and "colo colo"):
+    print("Tienes un gran futuro")
+elif 0>50 == "Catolica":
+    print("Siempre estaras segundo")
+elif 0>50 and "otro":
+    print("Lo siento mucho")
+elif 0<50 and "colo colo":
+    print("Has tenido una bella vida")
